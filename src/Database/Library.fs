8602779a4678e8f69e3856fea9761867842f48ba
youngen <youngen.simon@gmail.com>
2021-07-06 10:38:28 +0200

    added database projekt

---
+++ src/Database/Library.fs
@@ -0,0 +1,12 @@
+﻿module Database
+
+open Domain
+
+type Transaction<'a> = Transaction of 'a
+
+module Mock =
+    let addTask (task:OpenTask) : Transaction<OpenTask list> = Transaction [task]
+    let completeTask at (task:OpenTask) : Transaction<OpenTask list> = Transaction [] 
+    let removeTask (taskId:TaskId) : Transaction<OpenTask list> = Transaction []
+    let listOpenTasks () : Transaction<OpenTask list> = Transaction []
+    let listCompletedTasks () : Transaction<CompletedTask list> = Transaction []
